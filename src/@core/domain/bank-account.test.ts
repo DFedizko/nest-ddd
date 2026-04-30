6656c9f6a1162a33eacaadb603fa58586816bd69
DFedizko <fedizkopedro@gmail.com>
2026-04-29 21:24:43 -0300

    chore: improving bank account test names

--- src/@core/domain/bank-account.test.ts
+++ src/@core/domain/bank-account.test.ts
@@ -3,19 +3,19 @@ import { BankAccount } from './bank-account'
 describe('Bank Account', () => {
     let bankAccount: BankAccount
 
-    it('should create bank account', () => {
+    it('should create a bank account', () => {
         bankAccount = new BankAccount('1', 1000, '1111')
         expect(bankAccount.id).toBe('1')
         expect(bankAccount.balance).toBe(1000)
         expect(bankAccount.accountNumber).toBe('1111')
     })
 
-    it('should debit balance', () => {
+    it('should debit an account', () => {
         bankAccount.debit(500)
         expect(bankAccount.balance).toBe(500)
     })
 
-    it('should credit balance', () => {
+    it('should credit an account', () => {
         bankAccount.credit(500)
         expect(bankAccount.balance).toBe(1000)
     })
